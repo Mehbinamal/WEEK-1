e6fd9f51e6fffac61b2a995bad8736d0324f82db
Mehbinamal <mehbinamal6@gmail.com>
2025-05-06 15:14:57 +0530

    Login Function Implemented

---
+++ May-6/LoginSystem.py
@@ -0,0 +1,19 @@
+
+#Predefined Demo Data
+VALID_USERNAME = "admin"
+VALID_PASSWORD = "password123"
+
+#Login Function
+def login (username,password):
+    if username == VALID_USERNAME and password == VALID_PASSWORD:
+        print("Login successful! Welcome, " + username + ".")
+    else:
+        print("Error: Incorrect username or password. Please try again.")
+
+#Taking Input & calling Function
+try:
+    username = input("Username: ")
+    password = input("Password: ")
+    login(username,password)
+except Exception as e:
+    print ("An Error Occured : {e}")
